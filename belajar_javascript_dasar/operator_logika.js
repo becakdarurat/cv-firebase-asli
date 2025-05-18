424fc4236e51f586ed106a227320fec0f742fe88
becakdraurat <sofyant757@gmail.com>
2025-05-18 18:16:09 +0700

    "belajar javascript dasar"

---
+++ belajar_javascript_dasar/operator_logika.js
@@ -0,0 +1,7 @@
+let x = 6;
+console.log(x % 2 == 0);
+console.log((x % 2 == 0) && (x > 10));
+console.log(typeof(x));
+
+let bilangan = (x % 2 == 0) ? "genap" : "ganjil";
+console.log(bilangan);
---
+++ belajar_javascript_dasar/string.js
@@ -0,0 +1,19 @@
+let string1 = "Hello World";
+let string2 = 'Hello World';
+let string3 = `Hello World`;
+let string4 = new String("Hello World");
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+
+let jumlahAngkaString = 'sofyan tanjung';
+console.log(jumlahAngkaString.length);
+
+let y = 10;
+console.log(`Hello World`);
+console.log(`isi variabel y adalah ${y}`);
+console.log(typeof(y));
+
+
+
