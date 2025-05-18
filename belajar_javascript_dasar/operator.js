a9e42b83e235a8e171fa2de74aee2658dab9e6e8
becakdarurat <sofyant757@gmail.com>
2025-05-18 11:11:45 +0700

    "belajar Javascript"

---
+++ belajar_javascript_dasar/operator.js
@@ -0,0 +1,11 @@
+let a = 10 + 5;
+console.log(a); // 15
+let b = 10 % 2;
+console.log(b); // 0
+let c = (5 + 3) * 2 / 2 + 1 - 2;
+console.log(c); 
+var x ;
+console.log(x); // undefined
+console.log(10 == "10")
+console.log(10 != 10)
+console.log(10 == 15)
